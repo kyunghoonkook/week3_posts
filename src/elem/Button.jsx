45629439c59280c0954b0c7ea556530e0cdaea4f
kyunghoonkook <rnrrudgg68@gmail.com>
2022-10-15 01:42:44 +0900

    기본 elem 세팅

--- src/elem/Button.jsx
+++ src/elem/Button.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import styled, { css } from "styled-components";
 
-const Button = () => {
-  return <div>Button</div>;
+const Button = (props) => {
+  return <StButton {...props}>{props.children}</StButton>;
 };
 
 export default Button;
+
+const StButton = styled.button`
+  cursor: pointer;
+  background-color: #fff;
+  border: 1px solid #222;
+  border-radius: 10px;
+
+  ${({ size }) => {
+    switch (size) {
+      case "large":
+        return css`
+          width: 150px;
+        `;
+      case "medium":
+        return css`
+          width: 100px;
+        `;
+      case "small":
+        return css`
+          width: 80px;
+        `;
+      default:
+        return css`
+          width: 50px;
+        `;
+    }
+  }}
+`;
--- src/elem/Input.jsx
+++ src/elem/Input.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import styled from "styled-components";
 
-const Input = () => {
-  return <div>Input</div>;
+const Input = (props) => {
+  return <StInput {...props} required={true} />;
 };
 
 export default Input;
+
+const StInput = styled.input`
+  width: 300px;
+  border: 1px solid #444;
+  border-radius: 10px;
+  padding: 5px;
+`;
