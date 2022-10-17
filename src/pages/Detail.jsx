5fb414cbb66a32b17c1d1a65f104e361f479bbe2
Yeonwoo Ga <vjk4874@naver.com>
2022-10-17 18:30:51 +0900

    커밋

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -2,9 +2,33 @@ import React from "react";
 import Layout from "../components/Layout";
 import Home from "./Home";
 import styled from "styled-components";
+import Header from "../components/Header";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { __getPostById } from "../redux/modules/postSlice";
+import { useParams } from "react-router-dom/dist";
+
+
+
+
+
 
 const Detail = () => {
+
+
+
+const { id } = useParams();
+const dispatch = useDispatch()
+
+useEffect(()=>{
+  dispatch(__getPostById(id))
+},[])
+// const [details,Setdetails] = useSelector((state)=>state.);
+// console.log(details)
+
   return (
+    <>
+    <Header/>
     <StFullContainer>
       <StContainer>
         <StTitleandWriter>
@@ -35,7 +59,7 @@ const Detail = () => {
 
       </StContainer>
     </StFullContainer>
-
+    </>
   );
   }
 export default Detail;
--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import comments from "../modules/commentsSlice";
 import posts from "../modules/postsSlice";
+import post from "../modules/postSlice";
 
 const store = configureStore({
-  reducer: { comments, posts },
+  reducer: { comments, posts, post },
 });
 
 export default store;
